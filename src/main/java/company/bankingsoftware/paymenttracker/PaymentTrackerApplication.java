03efb0c6043f816f65e41c6fc20e43c753a2e3d5
david <david@prorocketeers>
2019-08-12 13:09:19 +0200

    created gradle project skeleton

---
+++ src/main/java/company/bankingsoftware/paymenttracker/PaymentTrackerApplication.java
@@ -0,0 +1,9 @@
+package company.bankingsoftware.paymenttracker;
+
+public class PaymentTrackerApplication {
+
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+    }
+
+}
